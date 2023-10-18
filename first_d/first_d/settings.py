a3a4b54953944d324ecfa6bca44d5a48acc840df
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-18 14:26:08 -0300

    Update settings.py

--- first_d/first_d/settings.py
+++ first_d/first_d/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'first_d.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
+        "NAME": os.getenv('DB_NAME')",
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASS'),
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
