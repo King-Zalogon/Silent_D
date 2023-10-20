0a27355371da2bfb77af05050707557540a3d2cb
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-20 15:32:28 -0300

    Started working on Models

--- first_d/core/models.py
+++ first_d/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=15, verbose_name='Character name')
+    concept = models.CharField(max_length=30, verbose_name='Character concept')
+    rules_system = models.CharField(max_length=10, verbose_name='System')
