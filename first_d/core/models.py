bd670c651f3106edab613c1ac123d2ec9635b942
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-20 15:56:55 -0300

    Reverted 2nd migration to unify on 1st

--- first_d/core/models.py
+++ first_d/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Character(models.Model):
-    name = models.CharField(max_length=15, verbose_name='Character name')
-    concept = models.CharField(max_length=30, verbose_name='Character concept')
-    rules_system = models.CharField(max_length=10, verbose_name='System')
+    name = models.CharField(max_length=15, verbose_name='Name')
+    concept = models.CharField(max_length=30, verbose_name='Concept', default='TBD')
+    rules_system = models.CharField(max_length=10, verbose_name='System', default='TBD')
+    age = models.IntegerField(verbose_name='Age', default=0)
