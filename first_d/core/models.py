1430c8044cdf91e938c04ac53e945d41d35a6d47
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-23 16:33:10 -0300

    Creating new models System n Chronicle

--- first_d/core/models.py
+++ first_d/core/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class RulesSystem(models.Model):
+    system_name = models.CharField(max_length=10, verbose_name="System", default='TBD', null=True)
+
+
+class Chronicle(models.Model):
+    title = models.CharField(max_length=30, verbose_name="Chronicle's title")
+    description = models.CharField(verbose_name='Synopsys', null=True)
+    gm = models.CharField(max_length=15, verbose_name='Storyteller')
+    start = models.DateField(verbose_name='Date of first session', null=True)
+    rules_system = models.ForeignKey(RulesSystem, on_delete=models.CASCADE, null=True)
+    ended = models.BooleanField(verbose_name="Finished?", default=False)
+
+
 class Character(models.Model):
     name = models.CharField(max_length=15, verbose_name="Name")
     concept = models.CharField(max_length=30, verbose_name="Concept", default='TBD')
-    rules_system = models.CharField(max_length=10, verbose_name="System", default='TBD', null=True)
+    rules_system = models.ForeignKey(RulesSystem, on_delete=models.CASCADE, null=True)
     growth = models.IntegerField(default=0, verbose_name="Current level or Exp")
     age = models.IntegerField(verbose_name="Age", default=0)
     creator = models.CharField(max_length=15, verbose_name="Creator's name", default='Zalo')
@@ -13,4 +26,5 @@ class Character(models.Model):
     alive = models.BooleanField("Is alive?", default=True)
     bio = models.CharField(max_length=300, verbose_name='Biography', null=True)
     portrait = models.URLField(verbose_name="Portraits' URL", null=True)
-    
+    chronicle = models.ForeignKey(Chronicle, on_delete=models.CASCADE, null=True)
+
