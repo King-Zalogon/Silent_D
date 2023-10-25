548611dc78b5f37b4352adadbafec2c98e742066
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-10-25 15:54:49 -0300

    Abstract and proxy models

--- first_d/core/models.py
+++ first_d/core/models.py
@@ -5,13 +5,33 @@ from django.db import models
 class RulesSystem(models.Model):
     system_name = models.CharField(max_length=10, verbose_name="System", default='TBD', null=True)
 
-class GameMaster(models.Model):
-    name = models.CharField(max_length=15, verbose_name="GM Name")
+
+class Human(models.Model):
+    name = models.CharField(max_length=15, verbose_name="Name")
+    active = models.BooleanField(default=True, verbose_name="Active")
+
+    class Meta:
+        abstract = True # I know this might be simplyfied but I trying abstract and proxy models
+
+
+class Creator(Human):
+    is_player = models.BooleanField(default=True, verbose_name="Is a Player")
+    is_gm = models.BooleanField(default=True, verbose_name="Is a Storyteller")
+
+
+class Player(Creator):
+    class Meta:
+        proxy = True
+
+
+class GameMaster(Creator):
+    availability = models.CharField(default="TBD", verbose_name="Availability", null=True)
+
 
 class Chronicle(models.Model):
     title = models.CharField(max_length=30, verbose_name="Chronicle's title")
     description = models.CharField(verbose_name='Synopsys', null=True)
-    gm = models.CharField(max_length=15, verbose_name='Storyteller')
+    gm = models.OneToOneField(GameMaster, on_delete=models.CASCADE, verbose_name="Storyteller")
     start = models.DateField(verbose_name='Date of first session', null=True)
     rules_system = models.ForeignKey(RulesSystem, on_delete=models.CASCADE, null=True)
     ended = models.BooleanField(verbose_name="Finished?", default=False)
@@ -23,12 +43,10 @@ class Character(models.Model):
     rules_system = models.ForeignKey(RulesSystem, on_delete=models.CASCADE, null=True)
     growth = models.IntegerField(default=0, verbose_name="Current level or Exp")
     age = models.IntegerField(verbose_name="Age", default=0)
-    creator = models.CharField(max_length=15, verbose_name="Creator's name", default='Zalo')
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE, null=True)
     is_player = models.BooleanField(verbose_name="Is a player's character?", default=True)
     alive = models.BooleanField("Is alive?", default=True)
     bio = models.CharField(max_length=300, verbose_name='Biography', null=True)
     portrait = models.URLField(verbose_name="Portraits' URL", null=True)
     chronicle = models.ForeignKey(Chronicle, on_delete=models.CASCADE, null=True)
 
-class GameMaster(models.Model):
-    name = models.CharField(max_length=15, verbose_name="GM Name")
