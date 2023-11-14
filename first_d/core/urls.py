fd1c378911ce29665b47fa8f501e12aa933930f6
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-11-13 21:15:07 -0300

    Added login html and permission groups

--- first_d/core/urls.py
+++ first_d/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/login', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.index, name='index'),
     path('char_form', views.contact, name="char_form"),
     path('npc_list/', views.npc_list, name='npc_list'),
